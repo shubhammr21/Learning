1fde8658cf33797cffaaf6f3e7ee4eef9a1b3cc7
Shubham <shubhammr21@gmail.com>
2022-01-19 19:59:27 +0530

    Handling 404 requests

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(shopRoutes);
 app.use(adminRoutes);
 
 
+// 404 not found handle
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
+
+
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 });
