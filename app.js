4fcd4d09ab64ed54971d9c27d00a7ee2c97f0fe7
Shubham <shubhammr21@gmail.com>
2022-01-19 19:47:40 +0530

    Handling routes and inputs

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use((req, res, next) => {
-    console.log('Request received');
+    // console.log('Request received');
     next(); // Allows the request to continue to the next middleware in line
 })
 
-app.use((req, res, next) => {
-    console.log('Another middleware');
+app.use("/add-product", (req, res, next) => {
+    // console.log('Add product page middleware');
+    res.send(`
+        <form action="/product" method="POST">
+            <input type="text" name="title" />
+            <button type="submit">Add Product</button>
+        </form>
+    `);
+})
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use("/", (req, res, next) => {
+    // console.log('Home page middleware');
     res.send('<h1>Hello World</h1>');
 })
 
