dc35a7cac038e9da9457af65cc403c08fe02b906
Shubham <shubhammr21@gmail.com>
2022-01-19 20:07:42 +0530

    Filtering Path

--- routes/admin.js
+++ routes/admin.js
@@ -5,14 +5,14 @@ const router = express.Router();
 router.get("/add-product", (req, res, next) => {
     // console.log('Add product page middleware');
     res.send(`
-        <form action="/product" method="POST">
+        <form action="/admin/add-product" method="POST">
             <input type="text" name="title" />
             <button type="submit">Add Product</button>
         </form>
     `);
 })
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 })
